bb878067ac29944485a2587de180126f544c2d16
Yin Da <yd219913@alibaba-inc.com>
2022-10-24 16:57:57 +0800

    Feat: add min-request-timeout args
    Signed-off-by: Yin Da <yd219913@alibaba-inc.com>

--- pkg/util/apiserver/options/server_run_options.go
+++ pkg/util/apiserver/options/server_run_options.go
@@ -27,19 +27,22 @@ import (
 
 // ServerRunOptions is the extension option for configuring the APIServer
 type ServerRunOptions struct {
-	RequestTimeout time.Duration
+	RequestTimeout    time.Duration
+	MinRequestTimeout int
 }
 
 func NewServerRunOptions() *ServerRunOptions {
 	defaults := server.NewConfig(serializer.CodecFactory{})
 	return &ServerRunOptions{
-		RequestTimeout: defaults.RequestTimeout,
+		RequestTimeout:    defaults.RequestTimeout,
+		MinRequestTimeout: defaults.MinRequestTimeout,
 	}
 }
 
 // ApplyTo set the params in server.RecommendConfig
 func (s *ServerRunOptions) ApplyTo(c *server.Config) error {
 	c.RequestTimeout = s.RequestTimeout
+	c.MinRequestTimeout = s.MinRequestTimeout
 	return nil
 }
 
@@ -49,6 +52,11 @@ func (s *ServerRunOptions) AddFlags(fs *pflag.FlagSet) {
 		"An optional field indicating the duration a handler must keep a request open before timing "+
 		"it out. This is the default request timeout for requests but may be overridden by flags such as "+
 		"--min-request-timeout for specific types of requests.")
+	fs.IntVar(&s.MinRequestTimeout, "min-request-timeout", s.MinRequestTimeout, ""+
+		"An optional field indicating the minimum number of seconds a handler must keep "+
+		"a request open before timing it out. Currently only honored by the watch request "+
+		"handler, which picks a randomized value above this number as the connection timeout, "+
+		"to spread out load.")
 }
 
 var defaultServerRunOptions = NewServerRunOptions()
